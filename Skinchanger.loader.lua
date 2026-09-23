22a4afbf705fce3b93f3a11c0671af7f04ed22df
Smi47 <checkramiejosephversus@gmail.com>
2026-09-22 20:50:08 -0700

    Update Skinchanger.loader.lua

--- Skinchanger.loader.lua
+++ Skinchanger.loader.lua
@@ -15,4 +15,4 @@ end
 
 script_key = "%KEY%"
 
-loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/f384e3cb37499c782b3e34767dcaa0274fbb8709f1b8bbcd3448e593b959d9cc.lua"))()
+loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/4bfedc5edcf048ea9ae361efb57070fa409bb6d425efc999d46cf82acde1ea2f.lua"))()
