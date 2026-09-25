f92df9ee3e54555cb919228d6db409de24843845
Smi47 <checkramiejosephversus@gmail.com>
2026-09-24 18:06:59 -0700

    Update Skinchanger.loader.lua

--- Skinchanger.loader.lua
+++ Skinchanger.loader.lua
@@ -1,1 +1,6 @@
-loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/4bfedc5edcf048ea9ae361efb57070fa409bb6d425efc999d46cf82acde1ea2f.lua"))()
+if game.PlaceId == 17625359962 then
+    loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/4bfedc5edcf048ea9ae361efb57070fa409bb6d425efc999d46cf82acde1ea2f.lua"))()
+else
+    game.Players.LocalPlayer:Kick("This script only works in Rivals!")
+end
+
