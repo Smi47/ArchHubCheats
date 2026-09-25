368d73aae8fa377bb45d522b10f212a13aa69055
Smi47 <checkramiejosephversus@gmail.com>
2026-09-24 18:07:43 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,7 @@
+if game.PlaceId == 17625359962 then
+    loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/1221f931ae2e512ba167cb625995518889276b6e833596e67fec8e42738a3189.lua"))())
+else
+    game.Players.LocalPlayer:Kick("This script only works in Rivals!")
+end
 
-loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/1221f931ae2e512ba167cb625995518889276b6e833596e67fec8e42738a3189.lua"))()
+
